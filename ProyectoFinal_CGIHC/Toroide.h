fdad356e03dbb8bd61f79ef194c31b54a2d0be9a
andreaUNAM <90221797+andreaUNAM@users.noreply.github.com>
2022-05-07 14:35:30 -0500

    Creación de la clase para el toroide

---
+++ ProyectoFinal_CGIHC/Toroide.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <vector>
+
+const int MAX_VERTICES =  100;
+class Toroide
+{
+public:
+	float vertices[MAX_VERTICES];
+	int radioInterior, radioExterior;
+
+	Toroide() {}
+};
+
